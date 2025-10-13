4f5f5b85b3af01cce8b05b967061618ceb5aab55
Mohamed Walid <160646589+Pro-MW@users.noreply.github.com>
2025-10-13 19:57:10 +0300

    Update Contact form to database.js

--- assets/js/Contact form to database.js
+++ assets/js/Contact form to database.js
@@ -1,44 +1,33 @@
-function submitForm(event) {
-  event.preventDefault();
-  var form = event.target;
-  var status = document.getElementById('status');
-  
-  var firstName = document.getElementById('inputFirstName').value;
-  var lastName = document.getElementById('inputLastName').value;
-  var email = document.getElementById('inputEmail').value;
-  var message = document.getElementById('message').value;
-  
-  if (firstName === '' || lastName === '' || email === '' || message === '') {
-    status.innerHTML = 'Please complete all fields.';
-    status.style.color = 'red';
-    return;
-  }
-  
-  status.innerHTML = 'Sending...';
-  status.style.color = 'green';
-  
-  var formData = new FormData(form);
-  fetch(form.action, {
-    method: form.method,
-    body: formData
-  })
-  .then(response => response.json()) // نحول الـ response إلى JSON
-  .then(data => {
-    if (data.result === 'success') {
-      status.innerHTML = 'Message sent successfully ✓✓';
-      status.style.color = 'green';
-      form.reset();
-    } else {
-      status.innerHTML = 'Oops! There was a problem sending the message.';
-      status.style.color = 'red';
+  function submitForm(event) { 
+      event.preventDefault(); 
+      var form = event.target; 
+      var status = document.getElementById('status'); 
+      var firstName = document.getElementById('inputFirstName').value; 
+      var lastName = document.getElementById('inputLastName').value; 
+      var email = document.getElementById('inputEmail').value; 
+      var message = document.getElementById('message').value; 
+      if (firstName === '' || lastName === '' || email === '' || message === '') { 
+        status.innerHTML = 'Please complete all fields.'; 
+        status.style.color = 'red'; 
+        return; // توقف عن إرسال النموذج إذا كانت الحقول غير مكتملة 
+      } 
+      status.innerHTML = 'Sending...'; 
+      status.style.color = 'green'; 
+      var formData = new FormData(form); 
+      fetch(form.action, { 
+        method: form.method, 
+        body: formData 
+      }) 
+      .then(function(response) { 
+        status.innerHTML = 'Message sent successfully ✓✓'; 
+        status.style.color = 'green'; 
+        form.reset(); 
+      }) 
+      .catch(function(error) { 
+        status.innerHTML = 'Oops! There was a problem sending the message.'; 
+        status.style.color = 'red'; 
+      }); 
     }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    status.innerHTML = 'Oops! There was a problem sending the message.';
-    status.style.color = 'red';
-  });
-}
 
 
 
@@ -46,3 +35,4 @@ function submitForm(event) {
 
 
   
+
